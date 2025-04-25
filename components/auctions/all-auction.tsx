d6877d428c251f0b7ee2e49bead3d8d30c6555b3
SIRsahed <sahedrahmanshakil@gmail.com>
2025-04-25 09:28:30 +0600

    auction start

---
+++ components/auctions/all-auction.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AllAuction() {
+  return (
+    <section className='container py-12'>
+      <div className="text-center pb-10">
+        <h2 className='text-[40px] text-[#645949] font-bold'>All Auction</h2>
+      </div>
+    </section>
+  )
+}
